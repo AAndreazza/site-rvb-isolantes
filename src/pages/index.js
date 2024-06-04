9599eae653dd363dd486dd77294e92a726cbbb42
AAndreazza <alanaandreazza26@hotmail.com>
2024-06-04 16:09:13 -0300

    linha de produtos done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import ProdutosInicial from '../components/ProdutosInicial'
 
 const Index = () => {
     return(
-        <div>
-        <Hero></Hero>
+        <div className='bg-[#041537]'> {/*bg-[#041537] [#273574]*/}
+        <Hero />
+        <ProdutosInicial />
         </div>
     )
 }
