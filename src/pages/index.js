d63547ebcec4b89586f5c5b9413934f839a4a2b1
AAndreazza <alanaandreazza26@hotmail.com>
2024-06-04 17:21:53 -0300

    empresa done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import ProdutosInicial from '../components/ProdutosInicial'
+import Sobre from '../components/Sobre'
+import Footer from '../components/Footer'
 
 const Index = () => {
     return(
         <div className='bg-[#041537]'> {/*bg-[#041537] [#273574]*/}
         <Hero />
         <ProdutosInicial />
+        <Sobre />
+        <Footer />
         </div>
     )
 }
